5de2791aec189d3697edcecedd89642b8363ed21
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 <l.leroty@gmail.com>
2022-09-08 20:56:46 +0200

    fix the task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        if i < 9 and j < 10:
+for i in range(10):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
             print("{}{}, ".format(i, j), end='')
 print()
