e69aba96702d208d41819c7af9127d170a941804
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 <l.leroty@gmail.com>
2022-09-08 19:17:07 +0200

    add the task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for i in range(1, 101):
     if i % 15 == 0:
-        print("FizzBuzz")
+        print("FizzBuzz ", end='')
     elif i % 3 == 0:
-        print("Fizz")
+        print("Fizz ", end='')
     elif i % 5 == 0:
-        print("Buzz")
+        print("Buzz ", end='')
     else:
-        print(i)
+        print(i, " ", end='')
