53ac4883ba2f1f2ad798982ec19d8e0f96407454
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 <l.leroty@gmail.com>
2022-09-12 22:28:12 +0200

    add the task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for c in range(len(matrix[row])):
-            print("{:d}".format(matrix[row][c]), end="")
+            print("{:d} ".format(matrix[row][c]), end="")
         print()
