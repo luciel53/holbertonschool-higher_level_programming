b588df5ae5a909a64a6299712879570d761079f0
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 <l.leroty@gmail.com>
2022-09-12 15:38:08 +0200

    add the task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
 
-    if idx < 0 and idx >= len(new_list):
+    if idx < 0 or idx >= len(new_list):
         return new_list
 
     new_list[idx] = element
