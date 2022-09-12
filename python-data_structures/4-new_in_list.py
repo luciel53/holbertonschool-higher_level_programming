7141eae9b4a768af5349fc746a2416197e0b21b1
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 <l.leroty@gmail.com>
2022-09-12 15:24:34 +0200

    add the task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = 0
     new_list = my_list.copy()
 
-    if idx < 0 and idx >= len(new_list):
+    if idx < 0 and idx > len(new_list):
         return None
     else:
         new_list[idx] = element
