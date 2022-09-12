5321a68fc979fed9122aee716c880f9cacafc9a0
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 <l.leroty@gmail.com>
2022-09-12 19:58:28 +0200

    add the task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,4 +4,7 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i] is not None:
             new_string = my_string.translate(translation)
-        return new_string
+            return new_string
+        else:
+            new_string = ""
+            return ""
