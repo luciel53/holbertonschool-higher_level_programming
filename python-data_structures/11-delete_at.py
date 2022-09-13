ee03c956aac01cff01885522611e412d4911f54f
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 <l.leroty@gmail.com>
2022-09-13 19:42:51 +0200

    fix the task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx >= 0 and idx < len(my_list):
-        for i in range(my_list[idx]):
-            del my_list[idx]
+        del my_list[idx]
     return (my_list)
