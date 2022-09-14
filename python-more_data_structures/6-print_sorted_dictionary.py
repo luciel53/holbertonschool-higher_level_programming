173859d0bedf41010e604a00cbbf1dadfcb25d10
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 <l.leroty@gmail.com>
2022-09-14 16:51:28 +0200

    add the task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-from re import A
-from unittest.util import sorted_list_difference
-
-
 def print_sorted_dictionary(a_dictionary):
     sorted_dico = sorted(a_dictionary.items())
     for key, value in sorted_dico:
