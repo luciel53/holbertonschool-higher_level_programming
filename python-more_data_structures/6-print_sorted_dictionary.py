bf3d32c5603e47cc11d97b52693c40baecd7d27d
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 <l.leroty@gmail.com>
2022-09-14 16:52:54 +0200

    fix comma

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dico = sorted(a_dictionary.items())
     for key, value in sorted_dico:
-        print("{} : {}".format(key, value))
+        print("{}: {}".format(key, value))
