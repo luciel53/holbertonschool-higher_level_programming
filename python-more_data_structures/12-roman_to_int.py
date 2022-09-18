c4b6ad20bca1f1ec8f8a5d8cb3662f276b02cec3
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 <l.leroty@gmail.com>
2022-09-18 18:40:13 +0200

    add the task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    sum = 0
+
+    for rom in reversed(roman_string):
+        numb = d[rom]
+        if sum < numb * 3:
+            sum += numb
+        else:
+            sum -= numb
+
+    return sum
