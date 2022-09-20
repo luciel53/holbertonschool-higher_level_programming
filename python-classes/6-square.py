17ace73e9d4e090606d4020c59b36590f1f9fbe0
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 <l.leroty@gmail.com>
2022-09-20 23:13:19 +0200

    fix task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -17,6 +17,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(position[0]) != int or type(position[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     # getter method
     @property
@@ -56,4 +58,6 @@ class Square:
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
