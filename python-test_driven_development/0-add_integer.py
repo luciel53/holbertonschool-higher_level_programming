9578a0d40698a70f63e16cd69d97852cadb7459d
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 <l.leroty@gmail.com>
2022-09-20 21:20:48 +0200

    fix the task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,13 +5,16 @@
 def add_integer(a, b=98):
     """This function adds 2 integers if float and int or raise an error"""
 
+    if type(a) is float:
+        a = int(a)
+
+    if type(b) is float:
+        b = int(b)
+        
     if a is None or (type(a) is not float and type(a) is not int):
         raise TypeError("a must be an integer")
 
     if type(b) is not float and type(b) is not int:
         raise TypeError("b must be an integer")
-    if type(a):
-        a = int(a)
-    if type(b):
-        b = int (b)
+
     return a + b
