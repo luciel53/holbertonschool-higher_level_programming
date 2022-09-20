e4a7758e051874f2d4f43ac80186076366db0bb9
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 <l.leroty@gmail.com>
2022-09-20 20:54:23 +0200

    add the task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for a function that adds 2 integers"""
+
+
+def add_integer(a, b=98):
+    """This function adds 2 integers if float and int or raise an error"""
+
+    if type(a) is not float and type(a) is not int:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not float and type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
