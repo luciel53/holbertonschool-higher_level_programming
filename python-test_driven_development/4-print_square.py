650efbf4478dd95e2338d57369347add5a267d89
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 <l.leroty@gmail.com>
2022-09-21 23:22:16 +0200

    add the task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for a function that prints a square with the character #."""
+
+
+def print_square(size):
+    """a function that prints a square with the character #."""
+    row = 0
+    columns = 0
+
+    for row in range(size):
+        for col in range(size):
+            print("#", end="")
+        print()
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if type(size) is not int and size < 0:
+        raise TypeError("size must be an integer")
