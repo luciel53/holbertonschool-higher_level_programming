ecc66415f7bd284a5494b7864435e365435f2888
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 <l.leroty@gmail.com>
2022-09-22 17:04:59 +0200

    fix the test file

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,19 +10,17 @@ def text_indentation(text):
     a function that prints a text with 2 new lines after each of these
     characters: ., ? and :
     """
+    c = 0
 
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    c = 0
-
     while c < len(text):
-
-        if text[c] in ['.', '?', ':']:
+        if text[c] == '.' or text[c] == '?' or text[c] == ':':
             print(text[c])
             print()
             c += 1
 
         else:
-            print(text[c], end='')
+            print(text[c], end="")
         c += 1
