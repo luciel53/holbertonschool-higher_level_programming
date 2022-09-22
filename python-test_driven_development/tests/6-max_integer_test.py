eacbd02609e52f2bf22769310a018fa2a95f6fd4
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 <l.leroty@gmail.com>
2022-09-22 20:07:37 +0200

    fix the test file

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,3 +10,18 @@ class TestMaxInteger(unittest.TestCase):
     def test_no_list(self):
         # Test empty list
         self.assertEqual(max_integer(""), None)
+
+    def test_one_element(self):
+        #test for only one element
+        elem = [2]
+        self.assertEqual(max_integer(elem), 2)
+
+    def test_all_negative_numb(self):
+        #test for only negative numbers in a list
+        neg_numb = [-5, -7, -3, -1]
+        self.assertEqual(max_integer(neg_numb), -1)
+
+    def test_one_negative_numb(self):
+        one_neg_numb = [8, -9, 3, 5]
+        self.assertEqual(max_integer(one_neg_numb), 8)
+
