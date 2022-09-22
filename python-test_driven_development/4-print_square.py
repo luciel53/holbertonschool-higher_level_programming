5d0298f50577b20c348c7d7b1e795dd76996ba2d
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 <l.leroty@gmail.com>
2022-09-22 10:00:11 +0200

    fix the task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,15 +7,15 @@ def print_square(size):
     row = 0
     columns = 0
 
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
     for row in range(size):
         for col in range(size):
             print("#", end="")
         print()
-
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-    if type(size) is not int and size < 0:
-        raise TypeError("size must be an integer")
