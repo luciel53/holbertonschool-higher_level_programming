3bd0762b093014a7ba4df95ebf52d7d47d33245e
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 <l.leroty@gmail.com>
2022-09-22 12:28:20 +0200

    fix the code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,16 +11,18 @@ def text_indentation(text):
     characters: ., ? and :
     """
 
-    c = 0
-
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    for c in range(len(text)):
+    c = 0
+
+    while c < len(text):
+
         if text[c] in ['.', '?', ':']:
             print(text[c])
             print()
             c += 1
+
         else:
-            print(text[c], end="")
-            c += 1
+            print(text[c], end='')
+        c += 1
