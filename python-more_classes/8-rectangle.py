bdaf9590f643c52cea317adae834732d45673997
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 <l.leroty@gmail.com>
2022-09-24 16:40:13 +0200

    fix area 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -73,16 +73,17 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if rect_1.area() == rect_2.area():
-            return rect_1
-
-        if rect_2.area() > rect_1.area():
-            return rect_2
-
-        if rect_1.area() > rect_2.area():
-            return rect_1
 
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
+
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+
+        if area_1 >= area_2:
+            return rect_1
+
+        if area_2 > area_1:
+            return rect_2
