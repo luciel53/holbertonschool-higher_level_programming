8b3f970ccae68056cc247c541cc78d327e884a4d
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 <l.leroty@gmail.com>
2022-09-27 17:19:30 +0200

    add the task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,4 +3,9 @@
 
 
 def inherits_from(obj, a_class):
+    """
+    a function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class ;
+    otherwise False
+    """
     return (type(obj) is not a_class)
