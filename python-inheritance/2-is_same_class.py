43a327800e50a1e8b9f22c89a9f4c0aa836c939b
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 <l.leroty@gmail.com>
2022-09-27 16:46:41 +0200

    add task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty mod"""
+
+
+def is_same_class(obj, a_class):
+    """
+    a function that returns True if the object is exactly an instance
+    of the specified class ; otherwise False
+    """
+    return (type(obj) is a_class)
