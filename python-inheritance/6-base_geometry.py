7063d78e9e5e2d451222346b61170f1bd4b638b4
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 <l.leroty@gmail.com>
2022-09-27 20:48:28 +0200

    add the task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""empty mod"""
+
+
+class BaseGeometry:
+    """Write an empty class BaseGeometry"""
+    def area(self):
+        """
+        Public instance method: def area(self): that raises an Exception
+        with the message area() is not implemented
+        """
+        raise Exception('area() is not implemented')
