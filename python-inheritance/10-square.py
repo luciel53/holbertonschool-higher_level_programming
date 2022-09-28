004043e09e856cfbe5034240120f86d0a83650d8
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 <l.leroty@gmail.com>
2022-09-28 13:25:51 +0200

    fix task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,42 +1,6 @@
 #!/usr/bin/python3
-"""empty mod"""
-
-
-class BaseGeometry:
-    """Write an empty class BaseGeometry"""
-    def area(self):
-        """
-        Public instance method: def area(self): that raises an Exception
-        with the message area() is not implemented
-        """
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """
-        Public instance method: def integer_validator(self, name, value):
-        that validates value
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """a class Rectangle that inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        """
-        Instantiation with width and height:
-        def __init__(self, width, height):
-        """
-        self.__height = height
-        self.__width = width
-
-    def area(self):
-        return self.__height * self.__width
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+"""mod"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
