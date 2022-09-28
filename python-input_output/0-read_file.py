3c852f5b1c86ae7d5bf5c62adfdade80b8eee246
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 <l.leroty@gmail.com>
2022-09-28 14:00:44 +0200

    add the task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""mod"""
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints it to stdout:
+    """
+    with open(filename, encoding="utf-8") as a_file:
+        print(a_file.read(), end="")
