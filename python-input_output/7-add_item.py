aeb513848c62d53e7728535ad0a9171607e8acd9
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 <l.leroty@gmail.com>
2022-09-30 18:32:52 +0200

    add task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+module to import arguments + json
+a script that adds all arguments to a Python list, and then save them to a file
+"""
 from sys import argv as av
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
