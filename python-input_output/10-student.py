08e4eabe354213bdb8fddf96c5917a6222ff6816
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 <l.leroty@gmail.com>
2022-09-30 20:45:03 +0200

    add task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Student:
+    """a class Student that defines a student by"""
+    def __init__(self, first_name, last_name, age):
+        self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def to_json(self, attrs=None):
+        """
+        Public method def to_json(self): that retrieves a dictionary
+        representation of a Student instance (same as 8-class_to_json.py)
+        """
+        d = {}
+        # if no attributes, return every keys and values
+        if attrs is None:
+            return self.__dict__
+        # else, if key is present return the value
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                d[key] = value
+        return d
