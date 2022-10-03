e59c5768b6448499960a852f24991080a5f48bac
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 <l.leroty@gmail.com>
2022-10-03 20:03:31 +0200

    fix task 7

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     a function that reads a text file (UTF8) and prints it to stdout:
     """
     with open(filename, encoding="utf-8") as a_file:
-        print(a_file.read(), end="")
+        print(a_file.read())
