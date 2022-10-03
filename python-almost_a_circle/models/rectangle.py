d469139df7c7d7e76ab130c8630a3dcbd83b2037
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 <l.leroty@gmail.com>
2022-10-03 15:48:13 +0200

    add the task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,6 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if (value) < 0:
             raise ValueError("y must be >= 0")
+
+    def area(self):
+        return self.__height * self.__width
