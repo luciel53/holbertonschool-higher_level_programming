df9d99d7aa3286dee33cd8b71361a42cc2c3996a
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 <l.leroty@gmail.com>
2022-10-03 12:34:44 +0200

    add the task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,9 +4,21 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Class Rectangle inherits from Base
+    Private instance attributes, each with its own public getter and setter:
 
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
-
+        """Class constructor: def __init__(self, width, height, x=0, y=0,
+        id=None):
+        Call the super class with id - this super call with use the logic of
+        the __init__ of the Base class
+        Assign each argument width, height, x and y to the right attribute
+        """
         self.__width = width
         self.__height = height
         self.__x = x
