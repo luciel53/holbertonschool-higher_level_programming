4a4bdd590a244d9101e0c7a0d67be0c3ef9979be
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 <l.leroty@gmail.com>
2022-10-06 16:55:26 +0200

    add test_save_to_file [] in Rectangle exists

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -129,14 +129,6 @@ class TestRectangle(unittest.TestCase):
             self.assertEqual(json.loads(file.read()), json.loads('[]'))
         os.remove("Rectangle.json")
 
-    def test_save_to_file_None(self):
-        """Test of Rectangle.save_to_file([]) in Rectangle exists"""
-        R = []
-        Rectangle.save_to_file('R')
-        with open("Rectangle.json", 'r') as file:
-            self.assertEqual(json.loads(file.read()), json.loads('[]'))
-        os.remove("Rectangle.json")
-
 
 if __name__ == "__main__":
     unittest.main()
