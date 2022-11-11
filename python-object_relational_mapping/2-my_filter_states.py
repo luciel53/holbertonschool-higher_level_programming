98a8548b31dccc567cdf12cb3063e8ed605accc2
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 <l.leroty@gmail.com>
2022-11-11 10:50:12 +0100

    add the task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE binary '{}'"
+                "ORDER BY states.id ASC".format(sys.argv[4]))
+    for states in cur.fetchall():
+        print(states)
+    cur.close()
+    db.close()
