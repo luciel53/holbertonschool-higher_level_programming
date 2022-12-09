f1d1917b34c6fb185249e76d089eb7a8888334d9
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 <l.leroty@gmail.com>
2022-12-09 21:55:01 +0100

    add task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-/* Analysis of string that is done in argument */
-const a = parseInt(process.argv[2]);
-/* if b is not a number, print 'Missin...' */
-if (isNaN(a)) {
-  console.log('1');
-} else {
-  function factorial (a) {
-    if (a > 0) {
-      console.log(factorial(a - 1) * a);
-    }
+
+function factorial (a) {
+  if (isNaN(a) || a === 1) {
+    return 1;
+  } else {
+    return factorial(a - 1) * a;
   }
-console.log(factorial(process.argv[2]));
 }
+console.log(factorial(parseInt(process.argv[2])));
