ff28b8df68acfb16d977a62acfbf472a810686b1
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 <l.leroty@gmail.com>
2022-12-09 23:47:35 +0100

    add task 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+	  this.width = w;
+	  this.height = h;
+
+	  if (w <= 0 || h <= 0) {
+      var obj = {};
+    }
+  }
+}
+module.exports = Rectangle;
