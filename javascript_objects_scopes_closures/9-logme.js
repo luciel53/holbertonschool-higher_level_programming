9d20b5ce5bd451a6724dc34fa4905c8d2b04b1fd
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 <l.leroty@gmail.com>
2022-12-10 21:57:51 +0100

    update the task9 / change the comment

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -2,7 +2,7 @@
 
 let arg = 0;
 exports.logMe = function (item) {
-	/* a function that prints the number of arguments already printed and
+/* a function that prints the number of arguments already printed and
 the new argument value. */
   console.log(arg + ': ' + item);
   arg += 1;
