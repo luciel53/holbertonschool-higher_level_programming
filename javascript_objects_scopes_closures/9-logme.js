a9669e76fbaac8a48fb9fcf4f0cf347155e43e51
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 <l.leroty@gmail.com>
2022-12-10 21:54:47 +0100

    add the task9

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let arg = 0;
+exports.logMe = function (item) {
+	/* a function that prints the number of arguments already printed and
+the new argument value. */
+  console.log(arg + ': ' + item);
+  arg += 1;
+};
