f25f7e6ba3021ed0234e47f37dbfdf67fcddbd65
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 <l.leroty@gmail.com>
2022-12-12 14:54:58 +0100

    add comm

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+/* a script that reads and prints the content of a file. */
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (data === undefined) {
