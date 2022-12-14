bc39c1927aed0f79d9ced083247a567feb5e49a5
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 <l.leroty@gmail.com>
2022-12-14 12:22:48 +0100

    add the task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* a script that prints the number of movies where the character
+“Wedge Antilles” is present. */
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+request(url, function (error, response, body) {
+  if (body === undefined) {
+    console.error('error:', error);
+  } else {
+    /* json to a javascript object */
+    const tojavasobj = JSON.parse(body);
+
+    /* count the number of movies (reduce adds) */
+    for (let i = 0; tojavasobj.results[i]; i++) {
+      if (tojavasobj.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
