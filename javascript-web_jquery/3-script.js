9fe2fe0fbd8477404fcaa973260b6c708ddd16fd
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 <l.leroty@gmail.com>
2022-12-16 00:06:07 +0100

    add task 3

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* a JavaScript script that updates the text color of the <header> element
+to red (#FF0000) */
+$('#red_header').click(function () {
+	$('header').addClass('red');
+});
