bfa7e0c67032d9643d973cc6b15d28131899ee94
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 <l.leroty@gmail.com>
2022-12-16 11:32:09 +0100

    add task 4

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -2,5 +2,5 @@
 /* a JavaScript script that updates the text color of the <header> element
 to red (#FF0000) */
 $('#red_header').click(function () {
-	$('header').css('color', '#FF0000');
+  $('header').css('color', '#FF0000');
 });
---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* a JavaScript script that toggles the class of the <header> element when
+the user clicks on the tag DIV#toggle_header */
+$('#toggle_header').click(function () {
+  $('header').toggleClass('red green');
+});
