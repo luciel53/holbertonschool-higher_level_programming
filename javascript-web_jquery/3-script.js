62332e1af4adf18a915f5d2f88e97a20f08de661
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 <l.leroty@gmail.com>
2022-12-16 12:10:43 +0100

    fix the semistandard + task 4

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* a JavaScript script that updates the text color of the <header> element
 to red (#FF0000) */
+const $ = window.$;
 $('#red_header').click(function () {
   $('header').addClass('red');
 });
--- javascript-web_jquery/4-script.js
+++ javascript-web_jquery/4-script.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* a JavaScript script that toggles the class of the <header> element when
 the user clicks on the tag DIV#toggle_header */
+const $ = window.$;
 $('#toggle_header').click(function () {
   $('header').toggleClass('red green');
 });
