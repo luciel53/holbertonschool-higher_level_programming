2b8a1b21704a28ed8e3fafa505c8eab126b09bab
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 <l.leroty@gmail.com>
2022-12-16 17:54:56 +0100

    add the task 6

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* a JavaScript script that updates the text of the <header> element to
+New Header!!! when the user clicks on DIV#update_header */
+const $ = window.$;
+$('DIV#update_header').click(function () {
+  $('header').replaceWith('New Header!!!');
+});
